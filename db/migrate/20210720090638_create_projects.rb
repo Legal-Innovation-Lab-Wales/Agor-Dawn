2cd0a2ce058454269b4d557ed78b2be16f3e3144
AlexW555 <871807@swansea.ac.uk>
2021-07-20 14:14:04 +0100

    Added Constraints and Initialisers to project seed. Removed test files

--- db/migrate/20210720090638_create_projects.rb
+++ db/migrate/20210720090638_create_projects.rb
@@ -2,10 +2,10 @@ class CreateProjects < ActiveRecord::Migration[6.1]
   def change
     create_table :projects do |t|
       t.belongs_to :user, null: false, foreign_key: true
-      t.string :name
-      t.string :description
-      t.boolean :public
-      t.integer :view_count
+      t.string :name, null: false
+      t.string :description, null: false
+      t.boolean :public, null: false, default: true
+      t.integer :view_count, null: false, default: 0
 
       t.timestamps
     end
