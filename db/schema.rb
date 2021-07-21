f18f64047255fc83133ff0f64fb19d8743d8532f
garethdandrews <garethdandrews@gmail.com>
2021-07-21 14:19:19 +0100

    Extracted navbar to separate view, added initial project index scss

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_145858) do
+ActiveRecord::Schema.define(version: 2021_07_20_090638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.string "description", null: false
+    t.boolean "public", default: true, null: false
+    t.integer "view_count", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,4 +51,6 @@ ActiveRecord::Schema.define(version: 2021_07_19_145858) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "projects", "users"
 end
