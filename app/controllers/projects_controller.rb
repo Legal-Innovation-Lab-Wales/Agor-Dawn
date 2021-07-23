bfca5a928beac2ac2146df835948ba91c3388e24
AlexW555 <871807@swansea.ac.uk>
2021-07-23 09:58:39 +0100

    Added Basic layout to index for testing, Added foundations for show individual projects
    Added Basic layout to index for testing as design still being worked on.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,18 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def show
+    @project = Project.find_by(id: params[:id])
+    if @project.nil?
+      redirect_to action: :index
+    end
+  end
+
+  private
+  def project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_back(fallback_location: root_path, flash: { error: 'Project event not found' })
+  end
 end
