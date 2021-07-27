4a44fcf17f4f0e09f9da53d1e053446ee1c7844e
garethdandrews <garethdandrews@gmail.com>
2021-07-27 16:02:08 +0100

    Fix for a tag underline and margin between cards

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2021_07_23_082749) do
     t.text "description", null: false
     t.boolean "public", default: true, null: false
     t.integer "view_count", default: 0, null: false
+    t.text "summary", null: false
+    t.integer "num_likes", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
@@ -55,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_07_23_082749) do
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
     t.text "bio"
+    t.text "avatar"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
