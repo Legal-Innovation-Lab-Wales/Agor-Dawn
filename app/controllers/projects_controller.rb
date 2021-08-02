4a3f1dc5cc665f7192fda495b6245ccb8452ee01
garethdandrews <garethdandrews@gmail.com>
2021-08-02 13:53:08 +0100

    Added flash messages, added logic to authenticate user when not viewing index or a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
   before_action :project_params, only: %i[create update]
 
   def index
@@ -8,7 +9,7 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.includes(:user, :comments).find(params[:id])
   rescue ActiveRecord::RecordNotFound
-    redirect_back(fallback_location: root_path, flash: { error: 'Project event not found' })
+    redirect_back(fallback_location: root_path, flash: { error: 'Project not found' })
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'users'
 
-  # authenticated :user do
-    resources :projects do
-      resources :comments, only: %i[create]
-    end
-  # end
-
-  # resources :projects, only: %i[index show]
+  resources :projects do
+    resources :comments
+  end
 
   root 'projects#index'
 end
