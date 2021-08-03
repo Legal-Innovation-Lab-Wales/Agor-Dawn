ffd7a27d427470e613c76e06b3d027a0bac069bb
AlexW555 <871807@swansea.ac.uk>
2021-08-03 11:42:14 +0100

    Added number of likes count and changed icon for unlike

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,28 +1,34 @@
 class ProjectsController < ApplicationController
+  # GET /projects
   def index
     @projects = Project.includes(:user)
   end
 
+  # GET /projects/:id
   def show
     @project = Project.includes(:user, :comments).find(params[:id])
+    @numOfLikes = Like.all.where("project_id = ?", @project.id).count
   rescue ActiveRecord::RecordNotFound
     redirect_back(fallback_location: root_path, flash: { error: 'Project event not found' })
   end
 
+  # PUT /projects/:id/like
   def like
-    #Getting the project
     @project = Project.all.find(params[:id])
-    #creating a like with that project and that users ID
     Like.create(user_id: current_user.id, project_id: @project.id)
-    #redirect back to project
     redirect_to project_path(@project)
+  rescue ActiveRecord::RecordNotFound
+    redirect_back(fallback_location: project_path, flash: {error: 'Something went wrong'})
   end
 
+  # DELETE /projects/:id/unlike
   def unlike
     @project = Project.all.find(params[:id])
     @like = Like.all.where("user_id =? AND project_id = ?", current_user.id, @project.id)
     Like.delete(@like)
     redirect_to project_path(@project)
+  rescue ActiveRecord::RecordNotFound
+    redirect_back(fallback_location: project_path, flash: {error: 'Something went wrong'})
   end
 
 end
