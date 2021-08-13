53dba3ee902177b29af4a832bfadea7385a76227
Ieuan Skinner <ieuanskinner@gmail.com>
2021-08-13 09:27:18 +0100

    AD-22 When resizing the window the state of the mobile search field is preserved so the times icon is removed/added as needed.

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,3 +1,9 @@
+import variables from '../../assets/stylesheets/variables.scss';
+
+const CSS = {
+  screenSm: variables.screensm
+}
+
 const search = document.querySelector('nav .search'),
       search_btn = search.querySelector('.search-icon'),
       search_input = search.querySelector('.search-input'),
@@ -19,10 +25,15 @@ search_btn.addEventListener('click', () => {
   let search_input_display = window.getComputedStyle(search_input).display
   if (search_input_display === 'block') { // Search button functions as intended
     search_function(search_input)
-  } else if (search_input_display === 'none') { // Search button reveals a search input on mobile
-    search_btn.classList.toggle('fa-search')
-    search_btn.classList.toggle('fa-times')
+  } else if (search_input_display === 'none') { // Search button reveals a search input on mobile'
     mobile_search.classList.toggle('hide')
+    if (mobile_search.classList.contains('hide')) {
+      search_btn.classList.add('fa-search')
+      search_btn.classList.remove('fa-times')
+    } else {
+      search_btn.classList.remove('fa-search')
+      search_btn.classList.add('fa-times')
+    }
   } else {
     alert('An error has occurred, please refresh your browser')
   }
@@ -30,4 +41,16 @@ search_btn.addEventListener('click', () => {
 
 mobile_search_btn.addEventListener('click', () => {
   search_function(mobile_search_input)
+})
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= parseInt(CSS.screenSm.replace('px', ''))) {
+    search_btn.classList.remove('fa-times')
+    search_btn.classList.add('fa-search')
+  } else {
+    if (!mobile_search.classList.contains('hide')) {
+      search_btn.classList.add('fa-times')
+      search_btn.classList.remove('fa-search')
+    }
+  }
 })
