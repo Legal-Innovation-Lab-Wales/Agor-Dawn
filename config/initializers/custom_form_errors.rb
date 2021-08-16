92ec20dc5b729d1d3444d37ede7bb55cdd1a0ccb
garethdandrews <garethdandrews@gmail.com>
2021-08-16 13:16:11 +0100

    Amended custom form errors for case with password confirmation field not showing an error correctly

--- config/initializers/custom_form_errors.rb
+++ config/initializers/custom_form_errors.rb
@@ -5,13 +5,21 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
 
   html = nil
   if field
-    value = field.attributes['type'].value
-    message = "#{value.capitalize} #{error_messages[value.to_sym][0]}"
     field['class'] = "#{field['class']} invalid"
+
+    value = field.attributes['name']
+                 .value
+                 .match(/\[(.*?)\]/)[0]
+                 .delete('[]')
+    
+    error_message = error_messages[value.to_sym][0]
+    error_message = 'is invalid' unless error_message.present?
 
     html = <<HTML
 #{fragment.to_s} 
-<p class=\"error\">#{message}</p>
+<p class=\"error\">
+#{value.capitalize} #{error_message}
+</p>
 HTML
     html
   else
