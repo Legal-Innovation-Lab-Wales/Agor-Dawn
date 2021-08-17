9853a9c009bf118ddf35bd3a805c25b8c39cb0c1
AlexW555 <871807@swansea.ac.uk>
2021-08-17 14:19:22 +0100

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,9 @@ class CommentsController < ApplicationController
   # POST /projects/:project_id/comments
   def create
     @project.comments.create!(comment: comment_params[:comment_text], user: current_user)
-    redirect_to project_path(@project), flash: { success: 'Comment was successfully submitted.' }
+    redirect_to project_path(@project), flash: { success: 'Comment submitted.' }
+  rescue ActiveRecord::RecordInvalid
+    redirect_to project_path(@project), flash: { error: 'Comment cannot be blank.' }
   end
 
   private
