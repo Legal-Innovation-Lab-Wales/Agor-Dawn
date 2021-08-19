10d55498b276fb7906944f108fee9d495e1147be
AlexW555 <871807@swansea.ac.uk>
2021-08-19 11:58:33 +0100

    Javascript cleanup

--- app/javascript/packs/liked_modal.js
+++ app/javascript/packs/liked_modal.js
@@ -1,27 +1,20 @@
-const liked_modal = document.getElementById("liked-modal")
-const open_liked_modal = document.getElementById("open-liked-modal")
-const close_liked_modal = document.getElementById("close-liked-modal")
-let backdrop = document.getElementById("backdrop")
-
-open_liked_modal.addEventListener("click", openModal)
-close_liked_modal.addEventListener("click", closeModal)
-
-function openModal() {
-    backdrop.style.display = "block"
-    liked_modal.style.display = "block"
-    liked_modal.classList.add("show")
-}
-
- function closeModal() {
-    backdrop.style.display = "none";
-    liked_modal.style.display = "none";
-    liked_modal.classList.remove("show");
-}
-
-// When the user clicks anywhere outside of the liked_modal, close it
-window.onclick = (event) => {
-    if (event.target === liked_modal) {
-        closeModal()
+const liked_modal = document.getElementById("liked-modal"),
+    open_liked_modal = document.getElementById("open-liked-modal"),
+    close_liked_modal = document.getElementById("close-liked-modal"),
+    backdrop = document.getElementById("backdrop"),
+    open_modal = () => {
+        backdrop.style.display = "block"
+        liked_modal.style.display = "block"
+        liked_modal.classList.add("show")
+    },
+    close_modal = () => {
+        backdrop.style.display = "none";
+        liked_modal.style.display = "none";
+        liked_modal.classList.remove("show");
     }
-}
 
+window.onclick = event => {
+    if (event.target === liked_modal) close_modal()
+}
+open_liked_modal.addEventListener("click", open_modal)
+close_liked_modal.addEventListener("click", close_modal)
