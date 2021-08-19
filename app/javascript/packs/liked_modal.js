08df6b2ce48fa28f6aeafcebc3830f2ae46086b1
AlexW555 <871807@swansea.ac.uk>
2021-08-19 11:46:58 +0100

    Update liked_modal.js

--- app/javascript/packs/liked_modal.js
+++ app/javascript/packs/liked_modal.js
@@ -1,23 +1,25 @@
 const liked_modal = document.getElementById("liked-modal")
 const open_liked_modal = document.getElementById("open-liked-modal")
 const close_liked_modal = document.getElementById("close-liked-modal")
+let backdrop = document.getElementById("backdrop")
 
 open_liked_modal.addEventListener("click", openModal)
 close_liked_modal.addEventListener("click", closeModal)
 
 function openModal() {
-    document.getElementById("backdrop").style.display = "block"
+    backdrop.style.display = "block"
     liked_modal.style.display = "block"
     liked_modal.classList.add("show")
 }
-function closeModal() {
-    document.getElementById("backdrop").style.display = "none"
-    liked_modal.style.display = "none"
-    liked_modal.classList.remove("show")
+
+ function closeModal() {
+    backdrop.style.display = "none";
+    liked_modal.style.display = "none";
+    liked_modal.classList.remove("show");
 }
 
 // When the user clicks anywhere outside of the liked_modal, close it
-window.onclick = function(event) {
+window.onclick = (event) => {
     if (event.target === liked_modal) {
         closeModal()
     }
