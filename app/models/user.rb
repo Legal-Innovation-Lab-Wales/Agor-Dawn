0f4c860366e6276c92eccdd5a29a3db39008c6c2
garethdandrews <garethdandrews@gmail.com>
2021-09-02 12:16:13 +0100

    Remove previous avatars, as long as they aren't default avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :trackable
   after_save :set_default_avatar
+  after_update :purge_previous_avatar, if: -> { avatar.changed? }
 
   has_many :comments
   has_many :projects
   has_many :likes
-  has_one_attached :avatar
+  has_one_attached :avatar, dependent: :detach
 
   scope :admins, -> { where(admin: true) }
   scope :non_admins, -> { where(admin: false) }
@@ -31,5 +32,9 @@ class User < ApplicationRecord
     return if avatar.attached?
 
     avatar.attach(ActiveStorage::Blob.where(default_avatar: true).order('RANDOM()').first)
+  end
+
+  def purge_previous_avatar
+    ActiveStorage::Blob.unattached.where(default_avatar: false).each(&:purge_later)
   end
 end
