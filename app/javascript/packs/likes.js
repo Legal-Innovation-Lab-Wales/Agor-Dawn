5647a9b3cd3046a6b5c83d7168ba813a65634310
garethdandrews <garethdandrews@gmail.com>
2021-09-06 12:11:31 +0100

    Visually updates like counts correctly

--- app/javascript/packs/likes.js
+++ app/javascript/packs/likes.js
@@ -3,7 +3,16 @@ const url = new URL(`${location.href}/likes`),
       headers = {'Content-Type': 'application/json', 'X-CSRF-Token': csrf_tokens.length > 0 ? csrf_tokens[0].content : ''},
       like_btn = document.querySelector('.like-count .like-button'),
       like_icon = like_btn.querySelector('i'),
-      like_count = document.querySelector('.like-count .count')
+      like_count = document.querySelector('.like-count .count'),
+      like_metadata = document.querySelector('.stats .metadata.metadata-likes'),
+      increment_counter = (count) => {
+        count.innerHTML = count.innerHTML.replace(/\d+/, parseInt(count.innerText) + 1)
+      },
+      decrement_counter = (count) => {
+        count.innerHTML = count.innerHTML.replace(/\d+/, parseInt(count.innerText) - 1)
+      }
+
+
 
 like_btn.addEventListener('click', () => {
   if (like_icon.classList.contains('far')) { // like
@@ -11,10 +20,11 @@ like_btn.addEventListener('click', () => {
       method: 'POST',
       headers: headers
     })
-    .then(data => {
+    .then(() => {
       like_icon.classList.remove('far')
       like_icon.classList.add('fas')
-      like_count.innerText = `${parseInt(like_count.innerText.match(/\d+/)[0]) + 1} likes`
+      increment_counter(like_count)
+      increment_counter(like_metadata)
     })
     .catch(e => {
       console.log(e);
@@ -24,10 +34,11 @@ like_btn.addEventListener('click', () => {
       method: 'DELETE',
       headers: headers
     })
-    .then(data => {
+    .then(() => {
       like_icon.classList.remove('fas')
       like_icon.classList.add('far')
-      like_count.innerText = `${parseInt(like_count.innerText.match(/\d+/)[0]) - 1} likes`
+      decrement_counter(like_count)
+      decrement_counter(like_metadata)
     })
     .catch(e => {
       console.log(e)
