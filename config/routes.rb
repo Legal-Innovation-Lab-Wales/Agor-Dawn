9fdf842aba37a784258b48de796a4ddb58423aeb
AlexW555 <871807@swansea.ac.uk>
2021-09-14 11:37:23 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'users'
+  devise_for :users, path: 'users', controllers: { registrations: 'user/registrations' }
 
   resources :users, only: :show
   resources :admin, only: :index do
