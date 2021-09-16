9b3e306aecabd7fdd6394c34dbd6a370ac818e13
AlexW555 <871807@swansea.ac.uk>
2021-09-16 10:36:28 +0100

    Updated admin_mailed to include link to admin page

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,6 +4,6 @@ class AdminMailer < ApplicationMailer
   def new_user_signup(user, admin)
     @admin = admin
     @user = user
-    mail(to: @admin.email, subject: 'New User Signup', body: 'A new user has signed up, sign in to approve them')
+    mail(to: @admin.email, subject: 'New User Signup', body: 'A new user has signed up, sign in to approve them https://agordawn.legaltech.wales/admin')
   end
 end
