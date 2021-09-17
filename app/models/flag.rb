726936bafc90b30df9194c777f53a2002f93d638
garethdandrews <garethdandrews@gmail.com>
2021-09-17 09:13:45 +0100

    Update flagged removed

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -3,15 +3,7 @@ class Flag < ApplicationRecord
   belongs_to :user
   belongs_to :flaggable, polymorphic: true
 
-  before_save :update_flagged
-
   validates_presence_of :user_id, :reason, :flaggable
 
   scope :most_recent, -> { order(created_at: :desc) }
-
-  private
-
-  def update_flagged
-    flaggable.update!(flagged: admin_resolved && user_resolved)
-  end
 end
