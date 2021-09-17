d2e766c70a7fbfc1b6fdd8f1431d082e15f32e8f
AlexW555 <871807@swansea.ac.uk>
2021-09-17 13:48:12 +0100

    Changed if !current_user.nil? to unless current_user.nil? & Fixed commenting in development enviroment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,8 @@ Rails.application.configure do
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
 
-  # Tell Active Support which deprecation messages to disallowconfig.active_support.disallowed_deprecation_warnings = []
+  # Tell Active Support which deprecation messages to disallow
+  config.active_support.disallowed_deprecation_warnings = []
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
