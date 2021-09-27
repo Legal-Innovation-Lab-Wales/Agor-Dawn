9d16de9d31f25df0e214f63334c1e3a362779858
Ieuan Skinner <ieuanskinner@gmail.com>
2021-09-27 11:51:59 +0100

    AD-48 - Changed is_flagged to say OR rather than AND and added dummy actions for the flag model.

--- app/models/concerns/flaggable_scope.rb
+++ app/models/concerns/flaggable_scope.rb
@@ -3,7 +3,7 @@ module FlaggableScope
   extend ActiveSupport::Concern
 
   included do
-    scope :is_flagged, ->(user_id) { joins(:flags).where("#{table_name}.user_id != ? and (flags.admin_resolved is False and flags.user_resolved is False)", user_id) }
+    scope :is_flagged, ->(user_id) { joins(:flags).where("#{table_name}.user_id != ? and (flags.admin_resolved is False or flags.user_resolved is False)", user_id) }
     scope :not_flagged, ->(user_id) { where.not(id: is_flagged(user_id)) }
 
     def flagged?
--- app/models/flag.rb
+++ app/models/flag.rb
@@ -4,7 +4,8 @@ class Flag < ApplicationRecord
   belongs_to :flaggable, polymorphic: true
 
   after_create :mail_user
-  # TODO: Add after_update (?) that appends to the reason and send mailer to admin/user depending on which resolved field flipped.
+  before_update :mail_admin, if: -> { !user_resolved_was && user_resolved }
+  before_update :mail_flag_resolved, if: -> { !admin_resolved_was && admin_resolved }
 
   validates_presence_of :flagged_by_id, :reason, :flaggable
 
@@ -14,5 +15,13 @@ class Flag < ApplicationRecord
 
   def mail_user
     FlagMailer.new_flag(flaggable.user, reason, flaggable_type.downcase, flaggable[:project_id] || flaggable.id)
+  end
+
+  def mail_admin
+    # Send mail to admin that user thinks they've solved it...
+  end
+
+  def mail_flag_resolved
+    # Send mail to user that the flag is considered resolved...
   end
 end
