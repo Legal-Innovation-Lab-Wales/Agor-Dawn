7e4d118f893f90a0ffb360422ab793dccdaabd26
Ieuan Skinner <ieuanskinner@gmail.com>
2021-10-05 12:41:26 +0100

    AD-48 - Updated the flag seed so that the mailer callback is skipped.

--- db/seeds/05_flags.rb
+++ db/seeds/05_flags.rb
@@ -1,15 +1,19 @@
 if Flag.count.zero?
   print "#{pretty_print_name('Flags')}\tStart: #{pretty_print(Time.now - @start_time)}"
 
+  Flag.skip_callback(:create, :after, :mail_user)
+
   Project.all.each do |project|
     next unless (project.id % 5).zero?
 
     Flag.create!(
-      flagged_by: User.where(admin: true).order('RANDOM()').first,
+      flagged_by: User.admins.order('RANDOM()').first,
       reason: "Admin: #{Faker::TvShows::TheITCrowd.quote}",
       flaggable: project
     )
   end
+
+  Flag.set_callback(:create, :after, :mail_user)
 
   puts "\tDuration: #{pretty_print(Time.now - @start_time)}   Elapsed: #{pretty_print(Time.now - @start_time)}"
   @last_time = Time.now
