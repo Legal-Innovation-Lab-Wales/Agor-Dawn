ba90d35b072cabc7925e8d02766242f1468aa9ba
garethdandrews <garethdandrews@gmail.com>
2021-10-05 13:30:03 +0100

    Updated admin_mailer so it uses a view, moved greetings from views to layout

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,9 +3,7 @@ class AdminMailer < ApplicationMailer
   def new_user_signup(user, admin)
     @admin = admin
     @user = user
-    mail(to: @admin.email, 
-         subject: 'New User Signup', 
-         body: "A new user, #{user.full_name} has signed up with the email \
-                address #{user.email}. Sign in to approve them #{admin_index_url}")
+
+    mail(to: @admin.email, subject: 'New User Signup')
   end
 end
