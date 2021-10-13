22ca00312ac40c09a18818b5aa69e9ac8004cfe5
garethdandrews <garethdandrews@gmail.com>
2021-10-13 14:47:08 +0100

    Added a mailer that notifies admins that a bug has been reported

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/bugs_controller.rb
 class BugsController < ApplicationController
-  before_action :bugs_params, only: :create
+  before_action :bug_params, only: :create
 
   def new
     render 'new'
@@ -8,6 +8,9 @@ class BugsController < ApplicationController
 
   def create
     # TODO: Send admins an email notifying them of a new bug
+    User.admins.each { |admin| BugsMailer.new_bug(admin, current_user, params[:summary], params[:description]).deliver_now }
+
+    redirect_back(fallback_location: new_bug_path, flash: { success: 'Thank you for your submission. Our team have been alerted and will begin investigating the issue.' })
   end
 
   private
