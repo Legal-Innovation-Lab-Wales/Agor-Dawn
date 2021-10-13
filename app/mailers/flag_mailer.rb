ab806a2a72ee36a384f4dada1426ae061fca98e7
garethdandrews <garethdandrews@gmail.com>
2021-10-13 14:29:49 +0100

    Added an instance variable to mailers called recipeient to make it clearer and to be used in the mailer layout

--- app/mailers/flag_mailer.rb
+++ app/mailers/flag_mailer.rb
@@ -1,30 +1,30 @@
 # app/mailers/flags_mailer.rb
 class FlagMailer < ApplicationMailer
   def new_flag(user, reason, type, project_id)
-    @user = user
+    @recipient = user
     @reason = reason
     @type = type
     @project_id = project_id
 
-    mail(to: @user.email, subject: "Your #{type} has been flagged for review")
+    mail(to: @recipient.email, subject: "Your #{type} has been flagged for review")
   end
 
   def user_resolved(admin, user, reason, type, project_id)
-    @admin = admin
+    @recipient = admin
     @user = user
     @reason = reason
     @type = type
     @project_id = project_id
 
-    mail(to: @admin.email, subject: 'A user has resolved a flag')
+    mail(to: @recipient.email, subject: 'A user has resolved a flag')
   end
 
   def admin_resolved(user, reason, type, project_id)
-    @user = user
+    @recipient = user
     @reason = reason
     @type = type
     @project_id = project_id
 
-    mail(to: @user.email, subject: 'Your flag has been resolved')
+    mail(to: @recipient.email, subject: 'Your flag has been resolved')
   end
 end
