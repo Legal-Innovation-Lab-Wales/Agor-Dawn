df4265c7522c459916b3c8436baa9d8c8a9b855a
garethdandrews <garethdandrews@gmail.com>
2021-10-14 12:37:26 +0100

    Bumped Ruby version to 2.7.4

--- config/routes.rb
+++ config/routes.rb
@@ -20,15 +20,5 @@ Rails.application.routes.draw do
     put 'reject', action: 'reject', on: :member, as: :reject
   end
 
-  resources :projects do
-    resources :comments, only: %i[create destroy]
-    resource :likes, only: %i[create destroy]
-  end
-
-  resources :flags, only: %i[index create] do
-    put 'resolve', action: 'resolve', on: :member, as: :resolve
-    put 'reject', action: 'reject', on: :member, as: :reject
-  end
-
   root 'pages#home', as: :authenticated_root
 end
